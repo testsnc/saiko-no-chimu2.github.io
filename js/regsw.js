96f1ceb2c2918addc27f0f731d9a1eb43d7c3e94
mashedmonk <79142547+mashedmonk@users.noreply.github.com>
2023-07-04 23:39:22 +0200

    rm

--- js/regsw.js
+++
@@ -1,9 +0,0 @@
-// CODELAB: Register service worker.
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-            .then((reg) => {
-                console.log('Service worker registered.', reg);
-            });
-    });
-}
