a45f7aa1514e9a926ca0dab9b9770728bdeec51d
testsnc <mashedmonk@gmail.com>
2023-07-04 22:14:33 +0000

    Update from https://github.com/testsnc/saiko-no-chimu/commit/fa046c21604085a13bdb2ce8875ef293aca75999

---
+++ js/regsw.js
@@ -0,0 +1,9 @@
+// CODELAB: Register service worker.
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then((reg) => {
+                console.log('Service worker registered.', reg);
+            });
+    });
+}
